2bf756c42851aae1474b2e66707d764069d5577a
GabrielMarmore <84024462+GabrielMarmore@users.noreply.github.com>
2022-12-09 17:46:27 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ListComponent } from './list/list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([{ path: 'list', component: ListComponent }]),
+    RouterModule.forRoot([{ path: '', component: ListComponent }]),
   ],
   declarations: [AppComponent, HelloComponent, ListComponent],
   bootstrap: [AppComponent],
