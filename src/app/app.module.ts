99931f97769b04d14b743063296f1f1071f3f5d9
GabrielMarmore <84024462+GabrielMarmore@users.noreply.github.com>
2022-12-09 17:48:02 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
+import { DoneListComponent } from './done-list/done-list.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([{ path: '', component: ListComponent }]),
+    RouterModule.forRoot([
+      { path: '', component: ListComponent },
+      { path: 'done', component: DoneListComponent },
+    ]),
   ],
   declarations: [AppComponent, HelloComponent, ListComponent],
   bootstrap: [AppComponent],
